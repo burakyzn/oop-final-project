87a686b45c8bec7fb3b876b95570db584301ebaf
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-04-28 01:00:07 +0300

    Create IHayvan.cs

---
+++ FinalProject/IHayvan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    interface IHayvan
+    {
+        void YemYe();
+        void UrunUret();
+        void EnerjiHarca();
+    }
+}
