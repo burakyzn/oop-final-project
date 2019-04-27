1ffbe14510ef0685c07445cec703f453eac445fc
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-04-28 01:00:02 +0300

    Update Ordek.cs

--- FinalProject/Ordek.cs
+++ FinalProject/Ordek.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace FinalProject
 {
-    class Ordek : Hayvanlar
+    class Ordek : Hayvanlar, IHayvan
     {
+        public Ordek()
+        {
+            HarcananEnerji = 3;
+        }
+
+        public void YemYe()
+        {
+            Enerji=100;
+        }
+
+        public void UrunUret()
+        {
+            if (Yasam == true)
+                UrunAdeti++;
+        }
+
+        public void EnerjiHarca()
+        {
+            if (Yasam == true)
+            {
+                if (Enerji >= 3)
+                    Enerji -= HarcananEnerji;
+                else
+                    Enerji -= Enerji;
+            }
+        }
     }
 }
