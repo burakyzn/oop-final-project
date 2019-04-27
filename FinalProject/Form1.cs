c3d0891be6fa3ca8086ea201a0f89b14da5a4584
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-04-28 01:47:18 +0300

    Update Form1.cs
    The form only fulfills the form checks.

--- FinalProject/Form1.cs
+++ FinalProject/Form1.cs
@@ -7,18 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace FinalProject
 {
     public partial class Form1 : Form
     {
-        Tavuk tavuk;
-        Ordek ordek;
-        Inek inek;
-        Keci keci;
-        int ciftlikZamani;
-        int kasaParasi;
-
+        Ciftlik yeniCiftligim;
         public Form1()
         {
             InitializeComponent();
@@ -26,148 +21,118 @@ namespace FinalProject
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            yeniCiftligim = new Ciftlik();
 
             tvkPrgBar.Value = 100;
             inekPrgBar.Value = 100;
             ordekPrgBar.Value = 100;
             KeciPrgBar.Value = 100;
 
-            tavuk = new Tavuk();
-            ordek = new Ordek();
-            inek = new Inek();
-            keci = new Keci();
-
-            ciftlikZamani = 0;
-            kasaParasi = 0;
-
         }
 
         private void tvkYemBtn_Click(object sender, EventArgs e)
         {
-            if(tavuk.Yasam == true)
-                tavuk.YemYe();
+            yeniCiftligim.tavukYemle();
         }
 
         private void inekYemBtn_Click(object sender, EventArgs e)
         {
-            if(inek.Yasam == true)
-                inek.YemYe();
+            yeniCiftligim.inekYemle();
         }
 
         private void keciYemBtn_Click(object sender, EventArgs e)
         {
-            if(keci.Yasam == true)
-                keci.YemYe();
+            yeniCiftligim.keciYemle();
         }
 
         private void ordekYemBtn_Click(object sender, EventArgs e)
         {
-            if(ordek.Yasam == true)
-                ordek.YemYe();
+            yeniCiftligim.ordekYemle();
         }
-
-        private void tvkTmr_Tick(object sender, EventArgs e)
-        {
-            tavuk.UrunUret();
-        }
-
-        private void inkTmr_Tick(object sender, EventArgs e)
-        {
-            inek.UrunUret();
-        }
-
-        private void ordkTmr_Tick(object sender, EventArgs e)
-        {
-            ordek.UrunUret();
-        }
-
-        private void keciTmr_Tick(object sender, EventArgs e)
-        {
-            keci.UrunUret();
-        }
-
-        private void ciftlikZaman_Tick(object sender, EventArgs e)
-        {
-            ciftlikZamani++;
-
-            tavuk.EnerjiHarca();
-            ordek.EnerjiHarca();
-            inek.EnerjiHarca();
-            keci.EnerjiHarca();
-        } 
 
         private void anlikOlaylar_Tick(object sender, EventArgs e)
         {
-            lblZaman.Text = ciftlikZamani.ToString();
-            lblKasa.Text = kasaParasi.ToString();
+            lblZaman.Text = yeniCiftligim.CiftlikZamani.ToString();
+            lblKasa.Text = yeniCiftligim.KasaParasi.ToString();
 
-            if(inek.Yasam == true)
+            if(yeniCiftligim.inek.Yasam == true)
             {
                 if (inekPrgBar.Value <= 0)
                 {
-                    inek.Yasam = false;
+                    yeniCiftligim.inek.Yasam = false;
                     lblInekYasam.Text = "ÖLÜ";
+
+                    SoundPlayer inekSes = new SoundPlayer();
+                    string path = Application.StartupPath.ToString() + "\\inek.wav";
+                    inekSes.SoundLocation = path;
+                    inekSes.Play();
                 }
                 else
-                {
-                    inekPrgBar.Value = inek.Enerji;
-
-                }
+                    inekPrgBar.Value = yeniCiftligim.inek.Enerji;
+                
             }
 
-            if(tavuk.Yasam == true)
+            if(yeniCiftligim.tavuk.Yasam == true)
             {
                 if (tvkPrgBar.Value <= 0)
                 {
-                    tavuk.Yasam = false;
+                    yeniCiftligim.tavuk.Yasam = false;
                     lblTavukYasam.Text = "ÖLÜ";
+
+                    SoundPlayer tavukSes = new SoundPlayer();
+                    string path = Application.StartupPath.ToString() + "\\tavuk.wav";
+                    tavukSes.SoundLocation = path;
+                    tavukSes.Play();
                 }
                 else
-                {
-                    tvkPrgBar.Value = tavuk.Enerji;
-                }
+                    tvkPrgBar.Value = yeniCiftligim.tavuk.Enerji;
+
             }
 
-            if (ordek.Yasam == true)
+            if (yeniCiftligim.ordek.Yasam == true)
             {
                 if (ordekPrgBar.Value <= 0)
                 {
-                    ordek.Yasam = false;
+                    yeniCiftligim.ordek.Yasam = false;
                     lblOrdekYasam.Text = "ÖLÜ";
+
+                    SoundPlayer ordekSes = new SoundPlayer();
+                    string path = Application.StartupPath.ToString() + "\\ordek.wav";
+                    ordekSes.SoundLocation = path;
+                    ordekSes.Play();
                 }
                 else
-                {
-                    ordekPrgBar.Value = ordek.Enerji;
+                    ordekPrgBar.Value = yeniCiftligim.ordek.Enerji;
 
-                }
             }
 
-            if (keci.Yasam == true)
+            if (yeniCiftligim.keci.Yasam == true)
             {
                 if (KeciPrgBar.Value <= 0)
                 {
-                    keci.Yasam = false;
+                    yeniCiftligim.keci.Yasam = false;
                     lblKeciYasam.Text = "ÖLÜ";
+
+                    SoundPlayer keciSes = new SoundPlayer();
+                    string path = Application.StartupPath.ToString() + "\\keci.wav";
+                    keciSes.SoundLocation = path;
+                    keciSes.Play();
                 }
                 else
-                {
-                    KeciPrgBar.Value = keci.Enerji;
-
-                }
+                    KeciPrgBar.Value = yeniCiftligim.keci.Enerji;
             }
 
-            lblTavukUrun.Text = tavuk.UrunAdeti.ToString();
-            lblKeciUrun.Text = keci.UrunAdeti.ToString();
-            lblOrdekUrun.Text = ordek.UrunAdeti.ToString();
-            lblInekUrun.Text = inek.UrunAdeti.ToString();
+            lblTavukUrun.Text = yeniCiftligim.urunAdetleri[0].ToString();
+            lblOrdekUrun.Text = yeniCiftligim.urunAdetleri[1].ToString();
+            lblKeciUrun.Text = yeniCiftligim.urunAdetleri[2].ToString();
+            lblInekUrun.Text = yeniCiftligim.urunAdetleri[3].ToString();
         }
 
         private void btnTavukSat_Click(object sender, EventArgs e)
         {
-            if(lblTavukUrun.Text != "0")
+            if (lblTavukUrun.Text != "0")
             {
-                tavuk.UrunAdeti--;
-                kasaParasi++;
+                yeniCiftligim.tavukYumurtasiSat();
             }
         }
 
@@ -175,8 +140,7 @@ namespace FinalProject
         {
             if(lblOrdekUrun.Text != "0")
             {
-                ordek.UrunAdeti--;
-                kasaParasi += 3;
+                yeniCiftligim.ordekYumurtasiSat();
             }
         }
 
@@ -184,8 +148,7 @@ namespace FinalProject
         {
             if (lblInekUrun.Text != "0")
             {
-                inek.UrunAdeti--;
-                kasaParasi += 5;
+                yeniCiftligim.inekSutuSat();
             }
         }
 
@@ -193,14 +156,15 @@ namespace FinalProject
         {
             if (lblKeciUrun.Text != "0")
             {
-                keci.UrunAdeti--;
-                kasaParasi += 8;
+                yeniCiftligim.KeciSutuSat();
             }
         }
 
-        private void tvkPrgBar_Click(object sender, EventArgs e)
-        {
-
+        private void ciftlikZamani_Tick(object sender, EventArgs e)
+        {   
+            yeniCiftligim.CiftlikZamani++;
+            yeniCiftligim.urunUret();
+            yeniCiftligim.enerjiHarca();
         }
     }
 }
