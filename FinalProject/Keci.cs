b849c1049287a566cb4df11191c4585a11d3bada
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-04-28 01:00:03 +0300

    Update Keci.cs

--- FinalProject/Keci.cs
+++ FinalProject/Keci.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace FinalProject
 {
-    class Keci : Hayvanlar
+    class Keci : Hayvanlar, IHayvan
     {
+        public Keci()
+        {
+            HarcananEnerji = 8;
+        }
+
+        public void YemYe()
+        {
+            Enerji = 100;
+        }
+
+        public void UrunUret()
+        {
+            if (Yasam == true)
+                UrunAdeti++;
+        }
+
+        public void EnerjiHarca()
+        {
+            if (Yasam == true)
+            {
+                if (Enerji >= 8)
+                    Enerji -= HarcananEnerji;
+                else
+                    Enerji -= Enerji;
+            }
+        }
     }
 }
