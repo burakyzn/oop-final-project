8db93ba8fb62373640bddcf809163e1d007e5836
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-05-04 13:40:25 +0300

    Update Hayvanlar.cs

--- FinalProject/Hayvanlar.cs
+++ FinalProject/Hayvanlar.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace FinalProject
 {
     abstract class Hayvanlar : IHayvan
