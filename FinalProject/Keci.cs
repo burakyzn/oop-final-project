50a3e9a8de3fc597239b52cbdbb298d8890c474e
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-05-04 13:39:56 +0300

    Update Keci.cs

--- FinalProject/Keci.cs
+++ FinalProject/Keci.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FinalProject
+﻿namespace FinalProject
 {
     class Keci : Hayvanlar
     {
